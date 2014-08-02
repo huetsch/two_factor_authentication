1a828f30d2fb6aec08a8c9238af7fbb30d0a439e
Mark Huetsch <markhuetsch@gmail.com>
2014-08-02 18:25:50 +0800

    drop the ?

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,5 +1,5 @@
 Warden::Manager.after_authentication do |user, auth, options|
-  if Devise.authenticate_on_login? and user.respond_to?(:need_two_factor_authentication?)
+  if Devise.authenticate_on_login and user.respond_to?(:need_two_factor_authentication?)
     if auth.session(options[:scope])[TwoFactorAuthentication::NEED_AUTHENTICATION] = user.need_two_factor_authentication?(auth.request)
       user.send_two_factor_authentication_code
     end
